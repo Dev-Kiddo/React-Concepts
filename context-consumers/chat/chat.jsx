544ecf84ee41b40f0f37863b3416b140afcef9c6
Raja Gopla R <109989163+Rajagopal9597@users.noreply.github.com>
2022-11-27 15:54:48 +0530

    Delete context-consumers directory

--- context-consumers/chat/chat.jsx
+++
@@ -1,6 +0,0 @@
-const Chat = ()=> {
-    return (
-        <div> I am chat component</div>
-    )
-}
-export default Chat;
--- context-consumers/post/post.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { NotificationContext } from "../../context/context";
-const Post = ()=> {
-    const {count} = useContext(NotificationContext)
-    return (
-        <div> I am Post component</div>
-    )
-}
-export default Post;
